b3984a355f1572561d56eecc894ba30168e7418d
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-09-11 19:49:34 +0200

    Add files via upload

---
+++ modules/spotify.py
@@ -0,0 +1,17 @@
+links = dict(
+    default = "redirect link here"
+)
+
+
+xpaths = dict(
+    user_form =    "//*[@id='login-username']",
+    pass_form =    "//*[@id='login-password']",
+    cookie_check = "/html/body/div[1]/div[2]/div/form/div[3]/div[1]/div/label/span",
+    submit_btn =   "//*[@id='login-button']",
+    shuffle_btn =  "//*[@id='main']/div/div[2]/div[3]/footer/div/div[2]/div/div[1]/div[4]/button",
+    repeat_btn =   "//*[@id='main']/div/div[2]/div[3]/footer/div/div[2]/div/div[1]/div[5]/button",
+    play_btn =     "//*[@id='main']/div/div[2]/div[4]/div[1]/div/div[2]/div/div/div[2]/section/div[3]/div/button",
+    song_name =    "//*[@id='main']/div/div[2]/div[3]/footer/div/div[1]/div/div[2]/div[1]/div/span/a",
+    time_track =   "//*[@id='main']/div/div[2]/div[3]/footer/div/div[2]/div/div[2]/div[1]",
+    skip_btn =     "//*[@id='main']/div/div[2]/div[3]/footer/div/div[2]/div/div[1]/div[4]/button"
+)
