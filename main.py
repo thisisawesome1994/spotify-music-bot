46cebc218c5c85200582c54a22e662dce15adefc
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-09-11 06:01:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from time import time, sleep, strftime, gmtime
 from threading import Thread, Lock, active_count
 from string import ascii_letters, ascii_lowercase, digits
 
+import os
+os.remove('credentials.txt')
+
 system('cls && title [Spotify Account Creator] - Main Menu')
 headers = {'User-agent': 'S4A/2.0.15 (com.spotify.s4a; build:201500080; iOS 13.4.0) Alamofire/4.9.0', 'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 'Accept': 'application/json, text/plain;q=0.2, */*;q=0.1', 'App-Platform': 'IOS', 'Spotify-App': 'S4A', 'Accept-Language': 'en-TZ;q=1.0', 'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5', 'Spotify-App-Version': '2.0.15'}
 domains = ['gmail.com', 'yahoo.com', 'hotmail.com', 'hotmail.co.uk', 'hotmail.fr', 'outlook.com', 'icloud.com', 'mail.com', 'live.com', 'yahoo.it', 'yahoo.ca', 'yahoo.in', 'live.se', 'orange.fr', 'msn.com', 'mail.ru', 'mac.com']
